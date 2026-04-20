389358bac80e3edf9e26def9489433aaaa98759c
Side-Of-Rice <baktaustdy@gmail.com>
2026-04-20 02:09:33 -0400

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,3 +1,91 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:35f6923df59cee54a1c011bb990cafb8a4ade76524e7f00141fdb31c3f7ff8ef
-size 2433
+import pandas as pd
+import numpy as np
+import pickle
+import torch
+import warnings
+from sklearn.metrics import classification_report
+from torch import nn
+
+def main():
+    warnings.filterwarnings("ignore")
+    
+    #Retrieve test dataset
+    try:
+        test_dict = torch.load('../data/artifacts/test_data.pt')
+    
+        #Retrieve trained BERT model
+        with open('../data/artifacts/bert_model.pkl', 'rb') as file:
+            bert = pickle.load(file)
+    except:
+        test_dict = torch.load('data/artifacts/test_data.pt')
+    
+        #Retrieve trained BERT model
+        with open('data/artifacts/bert_model.pkl', 'rb') as file:
+            bert = pickle.load(file)
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    test_size = 1000
+
+    # Move the model to the selected device (GPU or CPU)
+    bert.to(device)
+
+    # Get predictions for test data
+    with torch.no_grad():
+        # Move data to device and perform prediction
+        preds = bert(test_dict['test_seq'][:test_size].to(device), test_dict['test_mask'][:test_size].to(device))
+    
+        # Move the predictions to CPU for further processing
+        preds = preds.detach().cpu().numpy()
+
+    # Convert predictions to class labels (0 or 1 for binary classification)
+    pred = np.argmax(preds, axis=1)
+
+    # Print classification report
+    print(classification_report(test_dict['test_y'][:test_size], pred))
+    print(pred)
+# For pickel model
+class BERT_architecture(nn.Module):
+
+    def __init__(self, bert):
+
+      super(BERT_architecture, self).__init__()
+
+      self.bert = bert
+
+      # dropout layer
+      self.dropout = nn.Dropout(0.2)
+
+      # relu activation function
+      self.relu =  nn.ReLU()
+
+      # dense layer 1
+      self.fc1 = nn.Linear(768,512)
+
+      # dense layer 2 (Output layer)
+      self.fc2 = nn.Linear(512,2)
+
+      #softmax activation function
+      self.softmax = nn.LogSoftmax(dim=1)
+
+    #define the forward pass
+    def forward(self, sent_id, mask):
+
+      #pass the inputs to the model
+      _, cls_hs = self.bert(sent_id, attention_mask=mask, return_dict=False)
+
+      x = self.fc1(cls_hs)
+
+      x = self.relu(x)
+
+      x = self.dropout(x)
+
+      # output layer
+      x = self.fc2(x)
+
+      # apply softmax activation
+      x = self.softmax(x)
+
+      return x
+
+if __name__ == "__main__":
+    main()
