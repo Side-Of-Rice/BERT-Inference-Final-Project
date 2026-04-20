91cf1ddd0b131b0acc66f65941af0b0ce0a30676
Side-Of-Rice <baktaustdy@gmail.com>
2026-04-20 02:09:51 -0400

    Update data_validation.py

--- src/data_validation.py
+++ src/data_validation.py
@@ -1,3 +1,34 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f95f116f900f0e8da8dadd4e11ac2827b2c1bfd59b3e0bce28bf5f2803c425cf
-size 1236
+import pandas as pd
+
+def main():
+
+    # Import Raw Dataset
+    try:
+        RandR = pd.read_csv('../data/staged/ratings_and_reviews.csv')
+    except:
+        RandR = pd.read_csv('data/staged/ratings_and_reviews.csv')
+    
+    # Some comments are empty. Replace them with the empty string.
+    RandR['comments'] = RandR['comments'].fillna('').astype(str)
+    
+    # Check for Missing Variables
+    if(RandR.isnull().values.any()):
+        raise Exception("Table has missing values.")
+    
+    # Feature Engineering: Binary Target as "Top Performer" or "Under Performer" based on rating (rating >= 4.8)
+    RandR['performance'] = 'Under'
+    for index, row in RandR.iterrows():
+        if(row['review_scores_rating'] >= 4.80):
+            RandR.loc[index, 'performance'] = 'Top'
+            
+    # Save comment and performance to cleaned dataset
+    
+    try:
+        RandR[['comments', 'performance']].to_csv('../data/staged/cleaned_ratings_and_reviews.csv')
+        print('Dataset is Valid. Saving...')
+    except:
+        RandR[['comments', 'performance']].to_csv('data/staged/cleaned_ratings_and_reviews.csv')
+        print('Dataset is Valid. Saving...')
+        
+if __name__ == "__main__":
+    main()
